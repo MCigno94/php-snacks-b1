76b9c8d51bea34f088c8c8ae95a1aa74615e70f3
MCigno94 <mattiacigno27@gmail.com>
2022-05-20 12:45:49 +0200

    initial commit

---
+++ snak-1/index.php
@@ -0,0 +1,29 @@
+<!-- 
+## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+
+ -->
+
+<?php
+
+$match_schedule = [
+    [
+        "squadra_di_casa" => "Cantu",
+        "punti_casa" => 100,
+        "squadra_ospite" => "Milano",
+        "punti_ospite" => 90
+    ],
+    [
+        "squadra_di_casa" => "Pesaro",
+        "punti_casa" => 97,
+        "squadra_ospite" => "Trieste",
+        "punti_ospite" => 88
+    ],
+    [
+        "squadra_di_casa" => "Sassari",
+        "punti_casa" => 75,
+        "squadra_ospite" => "Tortona",
+        "punti_ospite" => 81
+    ],
+];
